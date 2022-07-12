35abfbf20f11b3acc58874f4379304a760f6e29d
Arthur Gabriel Gomes da Silva <98689718+Arthur-Gabriel-gs@users.noreply.github.com>
2022-07-12 11:36:07 -0300

    a

--- projeto-login/script.js
+++ projeto-login/script.js
@@ -4,5 +4,7 @@ const  moveOverlay = () => container.classList.toggle('move')
 
 
 document.querySelector('#open-register').addEventListener('click', moveOverlay)
-
 document.querySelector('#open-login').addEventListener('click', moveOverlay)
+
+document.querySelector('#open-register-mobile').addEventListener('click', moveOverlay)
+document.querySelector('#open-login-mobile').addEventListener('click', moveOverlay)
